164479f7e332b77364085c3c2f52004749c3f1de
mdrachuk <misha@drach.uk>
2019-08-20 22:44:12 +0300

    Fix report-list-item formatting

--- src/components/report-list/report-list-item.js
+++ src/components/report-list/report-list-item.js
@@ -2,42 +2,38 @@ import React, {Component} from 'react';
 import './report-list-item.css';
 
 
-export default  class ReportListItem extends Component {
+export default class ReportListItem extends Component {
 
-  render(){
+  render() {
     const {name, value, goal, id, onDeleted} = this.props;
 
-  return (
-       <span className='report-list-line'>
-              <span className="report-list-item-name" >
-                  {id} {}
-                  {name} {}
-                  {value}
-              </span>
+    return (
+      <span className='report-list-line'>
+        <span className="report-list-item-name">
+            {id} {}
+          {name} {}
+          {value}
+        </span>
 
 
-            <button type="button"
-                    className="btn btn-outline-warning btn-sm float-right"
-                    onClick={onDeleted}>
-                    <i className="fa fa-trash-o"/>
-            </button>
+        <button type="button"
+                className="btn btn-outline-warning btn-sm float-right"
+                onClick={onDeleted}>
+                <i className="fa fa-trash-o"/>
+        </button>
 
 
-           {goal ? (
+        {goal ? (
 
-                   <i className="fa fa-thumbs-up match"/>
+          <i className="fa fa-thumbs-up match"/>
 
-           ) : (
+        ) : (
 
-                   <i className="fa fa-thumbs-down notMatch "/>
+          <i className="fa fa-thumbs-down notMatch "/>
 
-           )
-           }
-
-
+        )}
        </span>
-        );
+    );
   };
-
 }
 
