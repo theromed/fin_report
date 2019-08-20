4385e9cecbe183bbf017a695ce82d14328bba4a1
mdrachuk <misha@drach.uk>
2019-08-20 22:58:24 +0300

    Rename ReportListItem to Coefficient

--- src/components/report-list/report-list.js
+++ src/components/report-list/report-list.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import ReportListItem from './report-list-item';
+import Coefficient from './report-list-item';
 import './report-list.css';
 
 const ReportList = ({report, onDeleted}) => {
 
   const coefficients = report.map(item => (
     <div key={item.id} className='list-group-item'>
-      <ReportListItem {...item}
-                      onDeleted={() => onDeleted(item.id)}
+      <Coefficient {...item}
+                   onDeleted={() => onDeleted(item.id)}
       />
     </div>
   ));
