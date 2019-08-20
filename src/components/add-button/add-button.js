43aed527ff53ac3c6b812ad8fb2922e3fbe8482e
mdrachuk <misha@drach.uk>
2019-08-20 23:04:34 +0300

    Format add-button.js

--- src/components/add-button/add-button.js
+++ src/components/add-button/add-button.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './add-button.css';
 
 const AddButton = () => {
-    return (
-        <button type="submit"
-                className="btn btn-outline-secondary btn-sm float-right">
-            Add coefficient!
-        </button>
-    )
+  return (
+    <button type="submit"
+            className="btn btn-outline-secondary btn-sm float-right">
+      Add coefficient!
+    </button>
+  )
 };
 export default AddButton;
