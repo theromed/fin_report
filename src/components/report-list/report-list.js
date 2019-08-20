34c38d9e7ee7692c65469e454db2d93830b867f6
mdrachuk <misha@drach.uk>
2019-08-20 22:50:47 +0300

    Fix formatting of report-list.js

--- src/components/report-list/report-list.js
+++ src/components/report-list/report-list.js
@@ -5,22 +5,21 @@ import './report-list.css';
 
 const ReportList = ({report, onDeleted}) => {
 
-    const elements = report.map((item) => {
-        return (
-            <div key={item.id} className='list-group-item'>
-                <ReportListItem { ... item}
-                onDeleted={()=>  onDeleted(item.id)}
-                />
-            </div>
-        );
-    });
-    return(
-        <ul className="list-group report-list">
-            {elements}
-        </ul>
+  const elements = report.map((item) => {
+    return (
+      <div key={item.id} className='list-group-item'>
+        <ReportListItem {...item}
+                        onDeleted={() => onDeleted(item.id)}
+        />
+      </div>
+    );
+  });
 
-
-    )
+  return (
+    <ul className="list-group report-list">
+      {elements}
+    </ul>
+  )
 };
 
 
